48e78d72ca5e3aed30606ba0f2822664b010427b
Melissa Steele <melissa.steele09@gmail.com>
2020-12-07 20:29:10 -0500

    Declare menu arrays

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ let mainDishButton = document.querySelector("#mainDish");
 let dessertButton = document.querySelector("#dessert");
 let entireMealButton = document.querySelector("#entireMeal");
 let letsCookButton = document.querySelector("#letsCookButton")
+
+let sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
+let mainDishes = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"];
+let desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava', 'Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
