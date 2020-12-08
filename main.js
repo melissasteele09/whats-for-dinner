b84f23f6819e5b6b2a9212c8c620c974f042d55d
Melissa Steele <melissa.steele09@gmail.com>
2020-12-07 20:15:10 -0500

    Assign initial query selectors

---
+++ main.js
@@ -0,0 +1,6 @@
+let addButton = document.querySelector("#addButton");
+let sideButton = document.querySelector("#side");
+let mainDishButton = document.querySelector("#mainDish");
+let dessertButton = document.querySelector("#dessert");
+let entireMealButton = document.querySelector("#entireMeal");
+let letsCookButton = document.querySelector("#letsCookButton")
