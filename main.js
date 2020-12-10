d50eeef7c6ce1e7b376b76287c10f924b7869fc8
Melissa Steele <melissa.steele09@gmail.com>
2020-12-10 17:37:32 -0500

    Add entire meal btn func.

--- main.js
+++ main.js
@@ -8,21 +8,22 @@ let titleDisplayBox = document.querySelector("#titleDisplay");
 
 let sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
 let mainDishes = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"];
-let desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava', 'Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+let desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+
 
 letsCookButton.addEventListener("click", displayFoodOption);
 
 function displayFoodOption() {
+let randomSidesIndex = Math.floor(Math.random() * sides.length);
+let randomMainDishIndex = Math.floor(Math.random() * mainDishes.length);
+let randomDessertIndex = Math.floor(Math.random() * desserts.length);
   if (sideButton.checked == true) {
-    let randomIndex = Math.floor(Math.random() * sides.length);
-    titleDisplayBox.innerHTML = `${sides[randomIndex]}`
+    titleDisplayBox.innerHTML = `You should make: <br> ${sides[randomSidesIndex]}!`
   } else if (mainDishButton.checked == true) {
-    let randomIndex = Math.floor(Math.random() * mainDishes.length);
-    titleDisplayBox.innerHTML = `${mainDishes[randomIndex]}`
+    titleDisplayBox.innerHTML = `You should make: <br> ${mainDishes[randomMainDishIndex]}!`
   } else if (dessertButton.checked == true) {
-      let randomIndex = Math.floor(Math.random() * desserts.length);
-      titleDisplayBox.innerHTML = `${desserts[randomIndex]}`
+    titleDisplayBox.innerHTML = `You should make: <br> ${desserts[randomDessertIndex]}!`
     } else if (entireMealButton.checked == true) {
-      console.log("ERROR")
+      titleDisplayBox.innerHTML = `You should make: <br> ${mainDishes[randomMainDishIndex]} with a side of ${sides[randomSidesIndex]} and ${desserts[randomDessertIndex]} for dessert!`;
     }
   }
