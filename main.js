065eebbc58ef714ed5b681d6bff185c44a1a8b63
Melissa Steele <melissa.steele09@gmail.com>
2020-12-09 19:59:17 -0500

    Set up initial side button func.

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ let sideButton = document.querySelector("#side");
 let mainDishButton = document.querySelector("#mainDish");
 let dessertButton = document.querySelector("#dessert");
 let entireMealButton = document.querySelector("#entireMeal");
-let letsCookButton = document.querySelector("#letsCookButton")
+let letsCookButton = document.querySelector("#letsCookButton");
+let titleDisplayBox = document.querySelector("#titleDisplay");
 
 let sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
 let mainDishes = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"];
-let desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava', 'Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"]
+let desserts = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava', 'Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+
+letsCookButton.addEventListener("click", displayFoodOption);
+
+function displayFoodOption() {
+  if(sideButton.checked == true) {
+    randomIndex = Math.floor(Math.random()*sides.length);
+    titleDisplayBox.innerHTML =`${sides[randomIndex]}`
+    }
+  // else if(mainDishButton.checked == true) {
+  //   for(let i=0; i < mainDishes.length; i++) {
+  //     let randomDish = mainDishes[i];
+  //     titleDisplayBox.innerHTML =`${randomDish}`
+  //   }
+  //   }else if(dessertButton.checked == true) {
+  //   for(let i=0; i < desserts.length; i++) {
+  //     let randomDessert = desserts[i];
+  //     titleDisplayBox.innerHTML =`${randomDessert}`
+  //   }
+  // } else if (entireMealButton.checked == true) {
+  //   console.log("ERROR")
+  // }
+}
+
+// put radio button names into array
+// iterate over array and if selected name is true
