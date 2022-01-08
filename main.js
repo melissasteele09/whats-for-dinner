9bd4b9e2a3ee1596fcb8b6f0b4b448b0a37c4e8b
Melissa Steele <melissa.steele09@gmail.com>
2022-01-07 17:01:43 -0700

    Update id names

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var sideOptions = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots",
-"Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"];
+  "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"
+];
 
 var mainDishOptions = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry",
-"Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen",
-"Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"];
+  "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen",
+  "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"
+];
 
 var dessertOptions = ["Apple Pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler",
-"Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie",
-"Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"];
+  "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Macaroons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie",
+  "Key Lime Pie", "Tart Tatin", "Croissants", "Eclairs"
+];
 
 var sideOptionButton = document.querySelector("#sideButton");
 var mainDishOptionButton = document.querySelector("#mainDishButton");
@@ -23,26 +26,26 @@ var random;
 
 cookButton.addEventListener("click", displayRecipe);
 
-function displayRecipe(){
-  if(sideOptionButton.checked) {
+function displayRecipe() {
+  if (sideOptionButton.checked) {
     random = Math.floor(Math.random() * sideOptions.length);
     recipeDisplayDiv.innerHTML =
-    `<h3 id="suggestionDisplay">You should make:</h3><article>${sideOptions[random]}</article>`;
-  } else if(mainDishOptionButton.checked) {
+      `<h3 class="suggestionDisplayTitle">You should make:</h3><article class="display">${sideOptions[random]}</article>`;
+  } else if (mainDishOptionButton.checked) {
     random = Math.floor(Math.random() * mainDishOptions.length);
     recipeDisplayDiv.innerHTML =
-    `<h3 id="suggestionDisplay">You should make:</h3><article>${mainDishOptions[random]}</article>`;
-  } else if(dessertOptionButton.checked) {
+      `<h3 class="suggestionDisplayTitle">You should make:</h3><article class="display">${mainDishOptions[random]}</article>`;
+  } else if (dessertOptionButton.checked) {
     random = Math.floor(Math.random() * dessertOptions.length);
     recipeDisplayDiv.innerHTML =
-    `<h3 id="suggestionDisplay">You should make:</h3><article>${dessertOptions[random]}</article>`;
-  } else if(entireMealOptionButton.checked) {
+      `<h3 class="suggestionDisplayTitle">You should make:</h3><article class="display">${dessertOptions[random]}</article>`;
+  } else if (entireMealOptionButton.checked) {
     randomSide = Math.floor(Math.random() * sideOptions.length);
     randomMain = Math.floor(Math.random() * mainDishOptions.length);
     randomDessert = Math.floor(Math.random() * dessertOptions.length);
     recipeDisplayDiv.innerHTML =
-    `<h3 id="suggestionDisplay">You should make:</h3><article>${mainDishOptions[randomMain]}
-    with a side of ${sideOptions[randomSide]} and ${dessertOptions[randomDessert]}
-    for dessert!</article>`;
+      `<h3 class="suggestionDisplayTitle">You should make:</h3><article class="display">${mainDishOptions[randomMain]}
+      with a side of ${sideOptions[randomSide]} and ${dessertOptions[randomDessert]}
+      for dessert!</article>`;
   }
-  };
+};
